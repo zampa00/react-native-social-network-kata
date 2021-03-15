5663d4ecb98ba5dc5dcca1a8e2d2a6ab9cf232b7
zampa00 <zampa92@gmail.com>
2021-03-15 14:13:15 +0100

    add another screen

---
+++ src/components/PublishMessage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const PublishMessage = () => (
+  <>
+    <Text>Publish message</Text>
+  </>
+);
+
+export default PublishMessage;
