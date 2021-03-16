0609980485a7a7ed309ca3152578212bda24f86b
zampa00 <zampa92@gmail.com>
2021-03-16 10:46:19 +0100

    add navigation base

--- App.tsx
+++ App.tsx
@@ -8,11 +8,21 @@
  * @format
  */
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Home from './src/components/Home';
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Home from '../src/components/Home';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<Home />);
 });
